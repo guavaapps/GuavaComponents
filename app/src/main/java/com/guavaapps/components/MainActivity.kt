41430be6ec64fef865eda8c3ea6ea5bf04609713
max <max@guavaapps.com>
2022-12-12 20:03:24 +0000

    commit

--- app/src/main/java/com/guavaapps/components/MainActivity.kt
+++ app/src/main/java/com/guavaapps/components/MainActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +21,13 @@ import com.guavaapps.components.listview.ListView
 class MainActivity : AppCompatActivity() {
     private val TAG: String = "MainActivity"
 
+    fun logColor (hct: Hct) {
+        val argb = Argb.from(hct.toInt())
+        with (argb) {
+            Log.e(TAG, "$alpha $red $green $blue")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,5 +36,20 @@ class MainActivity : AppCompatActivity() {
         Log.e(TAG, "listview - ${listView.canScroll}")
         listView.canScroll = false
         Log.e(TAG, "listview - ${listView.canScroll}")
+
+        val hct = Hct.fromInt(Color.RED)
+        hct.tone = 50f
+        logColor(hct)
+        hct.tone = 50f
+        logColor(hct)
+        hct.tone = 90f
+        logColor(hct)
+        hct.tone = 90f
+
+        val v = View(this)
+        v.layoutParams = ViewGroup.LayoutParams(-1, 200)
+        v.setBackgroundColor(hct.toInt())
+
+        listView.add(v)
     }
 }
